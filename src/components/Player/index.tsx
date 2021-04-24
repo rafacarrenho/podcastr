7a5d6080fdaf61e8e1bee707fef66f98fcab4b11
rafacarrenho <rafael.carrenho@hotmail.com>
2021-04-24 17:45:07 -0300

    Vercel

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -108,7 +108,6 @@ const Player = () => {
           </S.Slider>
           <span>{convertDurationToTimeString(episode?.duration ?? 0)}</span>
         </S.Progress>
-        {console.log(episode)}
         {episode && (
           <audio
             src={episode.url}
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,5 @@
-import axios from 'axios'
+import axios from "axios";
 
-export const api = axios.create({ baseURL: 'http://localhost:3333/' })
+export const api = axios.create({
+  baseURL: "https://podcastr-api-rafacarrenho.vercel.app/api/",
+});
